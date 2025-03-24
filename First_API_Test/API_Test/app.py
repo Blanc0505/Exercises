758f05724110d3de5572763f9054e9175780a993
Jaron L <leutholdjaron@gmail.com>
2025-03-24 20:02:31 +0100

    Added Delete Buttons

--- First_API_Test/API_Test/app.py
+++ First_API_Test/API_Test/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, url_for, redirect
 from members import members_api, members                                             # Import Blueprint + Liste
 
 
 app = Flask(__name__)
 app.register_blueprint(members_api)
 
-@app.route("/")  
+@app.route("/")  # http://127.0.0.1:5000
 def home():                                                                            
     return render_template('home.html')
 
@@ -20,6 +20,12 @@ def show_add_form():
 @app.route("/members/<string:name>/")  
 def getMember(name):                                                                   
     return name
+
+@app.route('/delete-member/<int:member_id>', methods=['POST'])
+def delete_member(member_id):
+    global members  # Wichtig, damit du die globale Liste veränderst
+    members = [m for m in members if m['id'] != member_id]
+    return redirect(url_for('show_members'))
 
 if __name__ == "__main__":
     app.run(debug=True)
